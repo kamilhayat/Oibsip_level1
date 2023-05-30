bee93bed167889e4fabdfde4ca15b1ec3c24a366
kamilhayat <119631915+kamilhayat@users.noreply.github.com>
2023-05-30 21:12:41 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer", "Software Developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+})
